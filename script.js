442affaafc8e69b30b8673cb2e4f94cc07514340
Rafael Brizuena <rafael.brizuena@gmail.com>
2023-02-10 10:19:50 -0400

    adding the initial structure of the script

---
+++ script.js
@@ -0,0 +1,47 @@
+const checkpointContainer = document.getElementById("checkpointContainer");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const checkpoints = document.querySelectorAll(".checkpoint")
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+    currentActive++;
+    
+    if(currentActive > checkpoints.length){
+        currentActive = checkpoints.length;
+    }
+
+    update();
+});
+
+prev.addEventListener("click", () => {
+    currentActive--;
+
+    if(currentActive < checkpoints.length){
+        currentActive = checkpoints.length;
+    }
+
+    update();
+});
+
+function update() {
+    checkpoints.forEach((checkpoint, idx) => {
+        if(idx < currentActive){
+            checkpoint.classList.add("active");
+        }else{
+            checkpoint.classList.remove("active");
+        }
+    })
+
+    const actives = document.querySelectorAll(".active");
+
+    if(currentActive === 1){
+        prev.disabled = true;
+    }else if(currentActive === 4){
+        next.disabled = true;
+    }else{
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
